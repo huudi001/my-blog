c3916b734ce5865120e571e95290ccb765a30a79
khalid hassan <abuuanas30@hotmail.com>
2017-11-06 00:06:07 +0300

    seting up database

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://khalid:pythonista@localhost/brownblog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://khalid:pythonista@localhost/codeblog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SIMPLEMDE_JS_IIFE = True
